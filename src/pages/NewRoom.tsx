f1570e00ac354147c080c23dfc80ecfb39e7c8fe
Natiene <cardosonatiene@gmail.com>
2021-06-22 20:28:11 -0300

    Created new pages

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+
+import { Link } from "react-router-dom";
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
+export function NewRoom() {
+  // const { user } = useAuth();
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        ></img>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main className="main-content">
+        <div>
+          <img src={logoImg} alt="Letmeask"></img>
+          <h2> Criar uma nova sala</h2>
+
+          <form>
+            <input type="text" placeholder="Nome da sala"></input>
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar numa sala existente? <Link to="/">clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
