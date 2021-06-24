57e754d05717014df178b97f6b758cca99d056c1
Natiene <cardosonatiene@gmail.com>
2021-06-23 21:49:23 -0300

    Created push method to insert datas in table of firebase

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,12 +1,37 @@
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
+import { FormEvent, useState } from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { database } from "../services/firebase";
+import userEvent from "@testing-library/user-event";
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
+
+  const history = useHistory();
+
+  const [newRoom, setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === "") {
+      return;
+    }
+
+    const roomRef = database.ref("rooms");
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
 
   return (
     <div id="page-auth">
@@ -23,8 +48,13 @@ export function NewRoom() {
           <img src={logoImg} alt="Letmeask"></img>
           <h2> Criar uma nova sala</h2>
 
-          <form>
-            <input type="text" placeholder="Nome da sala"></input>
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da sala"
+              onChange={(event) => setNewRoom(event.target.value)}
+              value={newRoom}
+            ></input>
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
